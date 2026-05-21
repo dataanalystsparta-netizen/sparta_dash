1bc854dab48dc79a834f51cba06a2b793032e3cc
dataanalystsparta-netizen <dataanalyst.sparta@gmail.com>
2026-05-21 16:16:46 +0530

    Update performance tips for call dispositions

--- agent_portal.py
+++ agent_portal.py
@@ -527,11 +527,21 @@ else:
         # --- REPLACED DISPOSITION TABLE WITH SIMPLE TRADING/DISPOSITION PERFORMANCE TIPS ---
         st.markdown("""
             <div class="tips-box">
-                <div class="tips-title">💡 Performance Tips: Correct Call Dispositions</div>
+                <div class="tips-title">💡 Performance Tips: Correct Call Dispositions and Data Quality</div>
                 <ul class="tips-list">
-                    <li><b>Answering Machines:</b> Do not dispose active customer connections as an "Answering Machine" if the Customer Talk Time exceeds 30 seconds.</li>
-                    <li><b>Data Accuracy:</b> Incorrectly marking live responses impacts future dialer data quality and limits overall reach.</li>
-                    <li><b>Guideline Check:</b> If a pitch is initiated or conversation details are collected, use the respective connected disposition matching the pitch result.</li>
+                    <li><b>Answering Machines:</b> Do not dispose active customer connections as an "Answering Machine" especially if the Customer Talk Time/connectivity exceeds 30 seconds.</li>
+                    <li><b>No Answer:</b> Dispose as "No Answer" only if the customer does not pick up the call</li>
+                    <li><b>Sky TV packages/Virgin:</b>Any call which indicates an error, should be disposed as "Sky TV packages" or "Virgin". They must not be disposed as as Answering Machines, Customer Hangup, No Answer, Not Interested etc. These dispositions would reappear in the dialler, and would dilute the quality of the data severly as the probability of the application of these customers is pretty low. </li>
+                    <li><b>Wrong Number:</b> Dispose them as "Wrong Number" if there is a mismatch in the data on the dialler and the data provided by the customer</li>
+                    <li><b>Family Interference/POA:</b>Dispose as Family Interference/POA, if a family member or a 3rd person takes care of the customers finances or other decisions.</li>
+                    <li><b>Dementia:</b>Dispose as Dementia, if the customer seems to have Dementia (seems forgetful of basic details), or seems Vulnerable.</li>
+                    <li><b>Over Age:</b>Dispose as Over Age if the customer is over 85 years old, or was born before 1940.</li>
+                    <li><b>Mobile Number:</b>Any number beginning with "7" should be disposed as a Mobile Number.</li>
+                    <li><b>Social Alarm VOIP:</b> If a customer has a Social Alarm/Medical Alarm/Careline/Lifeline etc, then use the disposition "Social Alarm VOIP"</li>
+                    <li><b>Hang up on bank details:</b> Use this disposition if the customer disconnects when hearing of or attempting any financial details.</li>
+                    <li><b>Busy:</b> If the customer is busy.</li>
+                    
+                    <u><b>Data Accuracy and Quality: The more accurate the disposition you enter, the better quality of the data would appear on the dialler for the entire team. If disposed correctly, the following dispositions would not reappear in the dialler: Dementia, Family Interference, POA, Sky TV Packages, Virgin, Over Age. The following dispositions would reappaear frequently on the dialler: Answering Machine, Customer Hangup, Interested, Callback etc. </b></u>
                 </ul>
             </div>
         """, unsafe_allow_html=True)
