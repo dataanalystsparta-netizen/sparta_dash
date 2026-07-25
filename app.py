b77884876647dd82ca096997115066286bf631a4
dataanalystsparta-netizen <dataanalyst.sparta@gmail.com>
2026-07-25 17:41:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -160,7 +160,7 @@ APPLICATION_SHEET = "Sparta"
 LIVE_SHEET = "Sparta2"
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
 
-NEW_ADVISORS = [
+NEW_ADVISORS = ["Subhodeep","Ravikant","Priyanshu","Kajal","Edwin"
     # Add advisor names here
 ]
 
